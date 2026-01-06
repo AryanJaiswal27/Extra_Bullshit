150ddf72dc12abb8ef57c6ad3a9ce2703bbf3ae0
AryanJaiswal27 <jaiswalaryan2708@gmail.com>
2026-01-06 19:08:01 +0530

    6

--- 6Jan26.java
+++ 6Jan26.java
@@ -133,4 +133,114 @@ class Solution {
         return result;
     }
 
+    public int maxFreqSum(String s) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int charValue = s.charAt(i);
+            freqMap.put(charValue, freqMap.getOrDefault(charValue, 0) + 1);
+        }
+
+        int vowelFreq = 0;
+        int consonantFreq = 0;
+        for (Map.Entry<Integer, Integer> iterable_element : freqMap.entrySet()) {
+            int key = iterable_element.getKey();
+            int value = iterable_element.getValue();
+            if (key == 'a' || key == 'e' || key == 'i' || key == 'o' || key == 'u') {
+                vowelFreq = Math.max(vowelFreq, value);
+            } else {
+                consonantFreq = Math.max(consonantFreq, value);
+            }
+
+        }
+
+        return vowelFreq + consonantFreq;
+
+    }
+
+    public int numJewelsInStones(String jewels, String stones) {
+        HashSet<Character> jewelSet = new HashSet<>();
+        for (int i = 0; i < jewels.length(); i++) {
+            jewelSet.add(jewels.charAt(i));
+        }
+
+        int count = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            if (jewelSet.contains(stones.charAt(i))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public int alternatingSum(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                sum += nums[i];
+            } else {
+                sum -= nums[i];
+            }
+        }
+        return sum;
+    }
+
+    public int findClosest(int x, int y, int z) {
+        return Math.abs(x - z) == Math.abs(y - z) ? 0
+                : Math.min(Math.abs(x - z), Math.abs(y - z)) == Math.abs(x - z) ? 1 : 2;
+    }
+
+    public String reversePrefix(String s, int k) {
+        String result = "";
+        for (int i = 0; i < k; i++) {
+            result = s.charAt(i) + result;
+        }
+        return result + s.substring(k);
+    }
+
+    public int maximumWealth(int[][] accounts) {
+        int m = accounts.length;
+        int n = accounts[0].length;
+        int maxWealth = 0;
+        for (int i = 0; i < m; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += accounts[i][j];
+            }
+            maxWealth = Math.max(maxWealth, sum);
+        }
+
+        return maxWealth;
+
+    }
+
+    public String convertDateToBinary(String date) {
+        String[] parts = date.split("-");
+        String result = "";
+        for (int i = 0; i < parts.length; i++) {
+            int x = Integer.parseInt(parts[i]);
+            String resultPart = "";
+            while (x != 0) {
+                if ((x & 1) == 1) {
+                    resultPart = "1" + resultPart;
+                } else {
+                    resultPart = "0" + resultPart;
+                }
+                x = x >> 1;
+            }
+            if (i == 0) {
+
+                result = resultPart;
+            } else {
+
+                result = result + "-" + resultPart;
+            }
+
+        }
+
+        return result;
+    }
+
 }
